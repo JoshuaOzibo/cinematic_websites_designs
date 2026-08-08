61d20cd2edb803d8d847943d0aa3165ad91bb270
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2026-08-09 00:26:56 +0100

    feat: implement UI form component and update project dependencies

--- kings_website/vite.config.ts
+++ kings_website/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
@@ -9,10 +8,6 @@ export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tailwindcss(),
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
     tanstackStart({
       server: { entry: "server" },
     }),
